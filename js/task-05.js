4ec690c94788bc30e21a67cbcf4dec86a9dc1d63
Sasha Shkapenko <sasha.shkapenko@gmail.com>
2022-02-18 15:36:20 +0200

    adds scripts

--- js/task-05.js
+++ js/task-05.js
@@ -3,7 +3,6 @@
 
 const inputEl = document.querySelector('#name-input');
 const outputEl = document.querySelector('#name-output');
-console.log(outputEl.textContent);
 
 inputEl.addEventListener('input', onInputChange);
 
